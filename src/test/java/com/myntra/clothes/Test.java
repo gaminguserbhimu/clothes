f60667e614a2fdf4feb26463c9fd11161e170edf
vinay <vinay@DESKTOP-TM236BS>
2021-05-08 08:49:16 +0530

    This is commited

---
+++ src/test/java/com/myntra/clothes/Test.java
@@ -0,0 +1,14 @@
+package com.myntra.clothes;
+	/**
+	 * Practice test
+	 * @author vinay
+	 *
+	 */
+public class Test {
+	
+	@org.testng.annotations.Test
+	public void hey(){
+		System.out.println("Hellow how are you");
+	}
+
+}
